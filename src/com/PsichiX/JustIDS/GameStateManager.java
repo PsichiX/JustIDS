fc9ac2a0ae036ad68d996ae1323b85616431dea8
Jarek Potiuk <jarek@potiuk.com>
2013-04-07 15:24:28 +0200

    Corrected logic of game

--- src/com/PsichiX/JustIDS/GameStateManager.java
+++ src/com/PsichiX/JustIDS/GameStateManager.java
@@ -6,8 +6,10 @@ import android.util.Log;
 
 public class GameStateManager {
 	
-	private volatile double lifePointsOfMine = 100.0;
-	private volatile double lifePointsOfOther = 100.0;
+	private boolean DEBUG_SELF_SENDING = false;
+	
+	private volatile double lifePointsOfMine;
+	private volatile double lifePointsOfOther;
 	
 	public double getLifePointsOfMine() {
 		return lifePointsOfMine;
@@ -32,7 +34,7 @@ public class GameStateManager {
 		this.bcm = bcm;
 		this.android_id = Secure.getString(context.getContentResolver(),Secure.ANDROID_ID); 
 		this.context = context;
-		
+		resetGame();
 		this.thread = new Thread() {
 			@Override
 			public void run() {
@@ -40,32 +42,48 @@ public class GameStateManager {
 				while (true) {
 					final byte[] message = GameStateManager.this.bcm.receiveBroadCast(GameStateManager.this.context);
 					PlayerInfo pi = (PlayerInfo) Serializer.deserialize(message);
-					if (!pi.id.equals(android_id)) {
+					if (shouldICare(pi)) {
 						Log.i("MSG","Received message from: " + pi);
 						lifePointsOfOther = pi.lifePoints;
-						if (pi.attackStrength > 0.01) {
+						if (isAttackSuccessfull(pi)) {
 							decreaseLifePointsOfMine(pi.attackStrength);
 							vibrateOnHit();
 							hitListener.run();
 						}
 					} else {
-						Log.i("MSG", "Skipping message from self: " + pi);
+						Log.i("MSG", "Skipping message : " + pi);
 					}
 					GameStateManager.this.onSomethingChanged.run();
 				}
+			}
+
+			private boolean shouldICare(PlayerInfo pi) {
+				if (hasFinished()) {
+					return false;
+				}
+				if (DEBUG_SELF_SENDING) {
+					return true;
+				}
+				return !pi.id.equals(android_id);
 			}
 
 		};
 		thread.start();
 	}
 
+	private boolean isAttackSuccessfull(PlayerInfo pi) {
+		return pi.attackStrength > 0.01;
+	}
+
 	private void vibrateOnHit() {
 		// TODO vibrate on hit
-		
 	}
 	
 	private synchronized double decreaseInternally(double decreaseBy) {
 		lifePointsOfMine -= decreaseBy;
+		if (lifePointsOfMine < 0.01) {
+			lifePointsOfMine =0.0;
+		}
 		return lifePointsOfMine;
 	}
 	
@@ -78,7 +96,15 @@ public class GameStateManager {
 		this.bcm.sendBroadcast(context, Serializer.serialize(pi));
 	}
 	
+	public boolean hasFinished() {
+		return (isLost() || isWon());
+	}
+	
 	public void attackWithStrength(double strength) {
+		if (hasFinished()) {
+			// do nothing
+			return;
+		}
 		double lp = decreaseInternally(0.0f);
 		PlayerInfo pi = new PlayerInfo();
 		pi.id = android_id; 
@@ -94,5 +120,11 @@ public class GameStateManager {
 	public void setHitListener(Runnable runnable) {
 		this.hitListener = runnable;
 	}
-
+	
+	public void resetGame(){
+		this.lifePointsOfMine = 100.0;
+		this.lifePointsOfOther = 100.0;
+		GameStateManager.this.onSomethingChanged.run();
+	}
+	
 }
